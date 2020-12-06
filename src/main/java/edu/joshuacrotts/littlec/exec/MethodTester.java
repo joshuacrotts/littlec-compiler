343fcc3badb1490c7eac040f94cd2acb96edeff4
Joshua Crotts <joshuacrotts@yahoo.com>
2020-12-06 17:28:19 -0500

    Fixed hex number range bug.

---
+++ src/main/java/edu/joshuacrotts/littlec/exec/MethodTester.java
@@ -0,0 +1,10 @@
+package edu.joshuacrotts.littlec.exec;
+
+import edu.joshuacrotts.littlec.main.LCUtilities;
+
+public class MethodTester {
+
+  public static void main(String[] args) {
+    System.out.println(LCUtilities.isValidIntLiteral("0xffff69b4"));
+  }
+}
